7bae8e79f41c71bd7d2ff0d73e998dc5df50258a
NhatLg <nate.m.luong@gmail.com>
2024-05-31 22:20:01 +0200

    refactor functions in main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def main() -> None:
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        wordcount = count_word(file_contents)
-        print(wordcount)
+    book_path = "books/frankenstein.txt"
+    text = get_text_from_book(book_path)
+    wordcount = count_word(text)
+    print(f"{wordcount} words found in the document")
 
 def count_word(text:str) -> int:
     words = text.split()
     return len(words)
+
+def get_text_from_book(path:str) -> str:
+    with open(path) as f:
+        return f.read()
 
 if __name__ == "__main__":
     main()
