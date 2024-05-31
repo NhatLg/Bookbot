43da1604233995be9126a60846fd7441a782a97a
NhatLg <nate.m.luong@gmail.com>
2024-05-31 22:30:19 +0200

    add new function in main.py to generate a frequency dictionary of char

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main() -> None:
     book_path = "books/frankenstein.txt"
     text = get_text_from_book(book_path)
     wordcount = count_word(text)
+    freq_char = generate_char_freq_dict(text.lower())
     print(f"{wordcount} words found in the document")
+    print(freq_char)
 
 def count_word(text:str) -> int:
     words = text.split()
@@ -11,6 +13,13 @@ def count_word(text:str) -> int:
 def get_text_from_book(path:str) -> str:
     with open(path) as f:
         return f.read()
+
+def generate_char_freq_dict(text:str) -> dict:
+    freq_dict = {}
+    for i in text:
+        freq_dict[i] = freq_dict.get(i, 0) + 1
+    return freq_dict
+
 
 if __name__ == "__main__":
     main()
